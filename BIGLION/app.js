30f12cdfcac00878cc17926c490bf635fbf8da61
Lena <evseeva.elena.1989@gmail.com>
2017-03-20 00:23:51 +0300

    add files for Biglion

---
+++ BIGLION/app.js
@@ -0,0 +1,66 @@
+
+function addSidebar() {
+	this.classList.add("hidden");
+	this.nextElementSibling.classList.remove("hidden");
+	document.querySelector('.small-menu__button').style.left = "75em";
+	document.querySelector('.sidebar').classList.remove("hidden");
+}
+
+function removeSidebar() {
+	this.classList.add("hidden");
+	this.previousElementSibling.classList.remove("hidden");
+	document.querySelector('.sidebar').classList.add("hidden");
+	document.querySelector('.small-menu__button').style.left = "86em";
+}
+
+function checkboxOff() {
+	this.classList.add("hidden");
+	this.nextElementSibling.classList.remove("hidden");
+	document.querySelector('.sidebar__menu2').style.color = "#b6aadc";
+}
+
+function checkboxOn() {
+	this.classList.add("hidden");
+	this.previousElementSibling.classList.remove("hidden");
+	document.querySelector('.sidebar__menu2').style.color = "#ffec00";
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function minusone() {
+	var amount = document.getElementsByName('amount-name')[0].value;
+	if(isNumeric(amount)) {
+		amount = Number(amount);
+		amount--;
+		document.getElementById('amount').value = amount;
+	}
+}
+
+function plusone() {
+	var amount = document.getElementsByName('amount-name')[0].value;
+	if(isNumeric(amount)) {
+		amount = Number(amount);
+		amount++;
+		document.getElementById('amount').value = amount;
+	}
+}
+
+document.querySelector('.img1').addEventListener('click', addSidebar);
+document.querySelector('.img2').addEventListener('click', removeSidebar);
+document.querySelector('.img3').addEventListener('click', checkboxOff);
+document.querySelector('.img4').addEventListener('click', checkboxOn);
+document.querySelector('.img4').addEventListener('click', checkboxOn);
+document.getElementById('minus').addEventListener('click', minusone);
+document.getElementById('plus').addEventListener('click', plusone);
+
+
+
+
+
+
+
+
+
+
