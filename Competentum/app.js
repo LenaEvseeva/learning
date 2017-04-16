641a826ce9bfddff8c25bd8d100f836c43c053c3
Lena <evseeva.elena.1989@gmail.com>
2017-04-16 22:53:39 +0300

    add new app.js for Competentum

--- Competentum/app.js
+++ Competentum/app.js
@@ -1,66 +1,149 @@
+var coorTop = 0;
+var coorLeft = 0;
+var INT_LEFT_MAX = 10;
+var INT_LEFT_MIN = -10;
+var arrConstTop = [10, -10];
 
-/*
-var start = Date.now(); // сохранить время начала
-
-var timer = setInterval(function() {
-  // вычислить сколько времени прошло с начала анимации
-  var timePassed = Date.now() - start;
-
-  if (timePassed >= 2000) {
-    clearInterval(timer); // конец через 2 секунды
-    return;
-  }
-
-  // рисует состояние анимации, соответствующее времени timePassed
-  draw(timePassed);
-
-}, 20);
-
-// в то время как timePassed идёт от 0 до 2000
-// left принимает значения от 0 до 400px
-function draw(timePassed) {
-  document.querySelector('.round').style.left = 5 + 'px';
-  document.querySelector('.round').style.top = timePassed / 5 + 'px';
-}
-
-*/
-
-var i = 0;
 function animation() {
-var timer = setInterval(function() {
-	
-		if(document.querySelector('.round').style.top == "440px") {
-			clearInterval(timer);
-			
-			var timertop = setInterval(function() {
-				if(document.querySelector('.round').style.top == "10px"){
-					clearInterval(timertop);
-					animation();
-				} else {
-					document.querySelector('.round').style.top = i + 'px';
-					document.querySelector('.round').style.left = i + 'px';
-					i = i - 10;
-				}
-			}, 50);
-			
-		} else {
-			document.querySelector('.round').style.left = i + 'px';
-			document.querySelector('.round').style.top = i + 'px';
-			i = i + 10;
-		}
- 
-	
-}, 50);
+    var CONST_LEFT = Math.random() * (INT_LEFT_MAX - INT_LEFT_MIN) + INT_LEFT_MIN;
+    var CONST_TOP = arrConstTop[Math.floor(Math.random() * (arrConstTop.length))];
+
+    if ((document.querySelector('.round').style.top == "") &&
+        (document.querySelector('.round').style.left == "")) {
+        CONST_LEFT = Math.abs(CONST_LEFT);
+        CONST_TOP = Math.abs(CONST_TOP);
+    }
+
+    if (CONST_TOP > 0) {
+        var animationPlusTop = setInterval(function() {
+
+            var CONST_LEFT_INDICATOR = Math.floor(parseInt(document.querySelector('.round').style.left));
+
+            if (CONST_LEFT_INDICATOR >= 440) {
+                CONST_LEFT = -(Math.abs(CONST_LEFT));
+            }
+
+            if (CONST_LEFT_INDICATOR <= 0) {
+                CONST_LEFT = Math.abs(CONST_LEFT);
+            }
+
+            if (document.querySelector('.round').style.top == "440px") {
+                clearInterval(animationPlusTop);
+
+                var animationMinusTop = setInterval(function() {
+
+                    if (document.querySelector('.round').style.top == "10px") {
+                        clearInterval(animationMinusTop);
+                        animation();
+                    } else {
+
+                        document.querySelector('.round').style.top = coorTop + 'px';
+                        document.querySelector('.round').style.left = coorLeft + 'px';
+                        coorTop = coorTop - CONST_TOP;
+                        coorLeft = coorLeft + CONST_LEFT;
+                        if (Math.floor(coorLeft) >= 450) {
+                            coorLeft = 450;
+                        }
+                        if (Math.floor(coorLeft) <= 0) {
+                            coorLeft = 0;
+                        }
+                        if (coorLeft == 450 || coorLeft == 0) {
+                            clearInterval(animationMinusTop);
+                            animation();
+                        }
+
+                    }
+                }, 50);
+
+            } else {
+
+                document.querySelector('.round').style.left = coorLeft + 'px';
+                document.querySelector('.round').style.top = coorTop + 'px';
+                coorTop = coorTop + CONST_TOP;
+                coorLeft = coorLeft + CONST_LEFT;
+                if (Math.floor(coorLeft) >= 450) {
+                    coorLeft = 450;
+                }
+                if (Math.floor(coorLeft) <= 0) {
+                    coorLeft = 0;
+                }
+                if (coorLeft == 450 || coorLeft == 0) {
+                    clearInterval(animationPlusTop);
+                    animation();
+                }
+
+            }
+
+
+        }, 50);
+    } else {
+
+
+
+        var animationPlusTopReverse = setInterval(function() {
+
+            var CONST_LEFT_INDICATOR = Math.floor(parseInt(document.querySelector('.round').style.left));
+
+            if (CONST_LEFT_INDICATOR >= 440) {
+                CONST_LEFT = -(Math.abs(CONST_LEFT));
+            }
+
+            if (CONST_LEFT_INDICATOR <= 0) {
+                CONST_LEFT = Math.abs(CONST_LEFT);
+            }
+
+            if (document.querySelector('.round').style.top == "10px") {
+                clearInterval(animationPlusTopReverse);
+
+                var animationMinusTopReverse = setInterval(function() {
+
+                    if (document.querySelector('.round').style.top == "440px") {
+                        clearInterval(animationMinusTopReverse);
+                        animation();
+                    } else {
+
+                        document.querySelector('.round').style.top = coorTop + 'px';
+                        document.querySelector('.round').style.left = coorLeft + 'px';
+                        coorTop = coorTop - CONST_TOP;
+                        coorLeft = coorLeft + CONST_LEFT;
+                        if (Math.floor(coorLeft) >= 450) {
+                            coorLeft = 450;
+                        }
+                        if (Math.floor(coorLeft) <= 0) {
+                            coorLeft = 0;
+                        }
+                        if (coorLeft == 450 || coorLeft == 0) {
+                            clearInterval(animationMinusTopReverse);
+                            animation();
+                        }
+
+                    }
+                }, 50);
+
+            } else {
+
+                document.querySelector('.round').style.left = coorLeft + 'px';
+                document.querySelector('.round').style.top = coorTop + 'px';
+                coorTop = coorTop + CONST_TOP;
+                coorLeft = coorLeft + CONST_LEFT;
+                if (Math.floor(coorLeft) >= 450) {
+                    coorLeft = 450;
+                }
+                if (Math.floor(coorLeft) <= 0) {
+                    coorLeft = 0;
+                }
+                if (coorLeft == 450 || coorLeft == 0) {
+                    clearInterval(animationPlusTopReverse);
+                    animation();
+                }
+
+            }
+
+
+        }, 50);
+
+
+    }
 }
- 
+
 document.querySelector('.round').addEventListener('click', animation);
-
-
-
-
-
-
-
-
-
-
